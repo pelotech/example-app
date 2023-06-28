1defb109042e186f51e4603f593b2bfdb2e34610
Sean Morton <seanmorton@hey.com>
2023-06-27 23:48:12 -0500

    Update response message

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, World!")
+		fmt.Fprintf(w, "Hello, Demo!")
 	})
 
 	http.ListenAndServe(":3000", nil)
