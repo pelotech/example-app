2a5c0d0577504b0dc16db9cc67200ed0f6368b66
Sean Morton <seanmorton@hey.com>
2023-06-28 09:38:39 -0500

    Revert "Update response message"
    This reverts commit 1defb109042e186f51e4603f593b2bfdb2e34610.

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, Demo!")
+		fmt.Fprintf(w, "Hello, World!")
 	})
 
 	http.ListenAndServe(":3000", nil)
